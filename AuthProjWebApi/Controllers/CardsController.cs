58743ebb110d4bd5a0596fd87b956e8eeeca9dea
LshMakh <157807032+LshMakh@users.noreply.github.com>
2024-10-02 13:46:12 +0400

    Add files via upload

---
+++ AuthProjWebApi/Controllers/CardsController.cs
@@ -0,0 +1,116 @@
+﻿using AuthProjWebApi.Models;
+using AuthProjWebApi.Packages;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace AuthProjWebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+
+    public class CardsController : ControllerBase
+    {
+        IPKG_CARD package;
+
+
+        public CardsController(IPKG_CARD package)
+        {
+            this.package = package;
+        }
+
+        List<Card> cards;
+
+        [HttpGet]
+        public IActionResult GetCards() {
+
+            List<Card> cards = new List<Card>();
+            try
+            {
+                cards = package.GetCards();
+            }
+            catch (Exception ex) {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+
+            }
+            return StatusCode(StatusCodes.Status200OK, cards);
+        }
+
+        [HttpPost]
+
+        public IActionResult SaveCard(Card card)
+        {
+
+            try
+            {
+                package.SaveCard(card);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            return StatusCode(StatusCodes.Status200OK);
+
+        }
+
+
+
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCard(int id)
+        {
+            try
+            {
+                package.DeleteCard(id);
+                return StatusCode(StatusCodes.Status200OK);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult GetCardbyId(int id) 
+        {
+
+            Card card;
+
+            try
+            {
+                card = package.GetCardbyId(id);
+            }
+            catch (Exception ex) 
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+
+            return StatusCode(StatusCodes.Status200OK, card);
+
+        }
+
+        [HttpPut("{id}")]
+
+        public IActionResult UpdateCard(int id,[FromBody] Card card)
+        {
+
+            try
+            {
+                package.UpdateCard(card);
+                return Ok();
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+
+        }
+
+    }
+}
+
+
